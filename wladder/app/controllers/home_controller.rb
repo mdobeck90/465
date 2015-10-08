407a0d14cee904dda4a555633a4c264c97fc0232
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-10-07 17:21:03 -0700

    Almost done, just need to make sure the blank guesses do not show up on the results view

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
-load "#{Rails.root}/lib/ladder.rb"
-load "#{Rails.root}/lib/words4.rb"
-load "#{Rails.root}/lib/words5.rb"
+ load "#{Rails.root}/lib/ladder.rb"
  def index
   @start_word = dictionary.sample 
   @end_word = dictionary.sample 
@@ -13,8 +11,34 @@ load "#{Rails.root}/lib/words5.rb"
   @step3 = params[:step3]
   @step4 = params[:step4]
   @step5 = params[:step5]
-
   @start_word = params[:start_word]
   @end_word =  params[:end_word]
+
+  @steps = [params[:step1]]
+  @steps = [params[:step2]]
+  @steps = [params[:step3]]
+  @steps = [params[:step4]]
+  @steps = [params[:step5]]
+
+  @win_state = true
+
+=begin
+  @steps.each do |word1, word2|
+   if edit_distance_of_one(word1,word2) == false
+    @win_state = false
+   end
+  end 
+=end
+if edit_distance_of_one(@step1, @step2) == false
+ @win_state = false
+elsif edit_distance_of_one(@step3, @step4) == false
+ @win_state = false
+elsif edit_distance_of_one(@step4, @step5) == false
+ @win_state = false
+end
+
+  if legal(@steps) == false
+    @win_state = false
+  end
  end
 end
