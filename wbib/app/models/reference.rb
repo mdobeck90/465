b5c89a73f4391afb80ddf5079519f155f2833e0b
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-10-24 21:28:05 -0700

    HTTP will now be prefixed to all URLs via the before_save function.  Will handle cases of HTTPS as well.

--- wbib/app/models/reference.rb
+++ wbib/app/models/reference.rb
@@ -1,7 +1,15 @@
 class Reference < ActiveRecord::Base
 
+  before_save :http_concat
+
   belongs_to :topic
   
   validates :url, :description, presence: true
+
+  def http_concat
+    if ("#{url}".starts_with?("http://")) == false && ("#{url}".starts_with?("https://")) == false
+      self.url = "http://" + "#{url}"
+    end
+  end
 
 end
