b608cc4d418687f6f44516f5b04dac18f4e2d89c
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-10-24 19:13:42 -0700

    Fixed user enter empty strings.  Validation for presence now required in all models

--- wbib/app/models/reference.rb
+++ wbib/app/models/reference.rb
@@ -1,5 +1,7 @@
 class Reference < ActiveRecord::Base
 
   belongs_to :topic
+  
+  validates :url, :description, presence: true
 
 end
--- wbib/app/models/topic.rb
+++ wbib/app/models/topic.rb
@@ -2,4 +2,6 @@ class Topic < ActiveRecord::Base
 
   has_many :references, dependent: :destroy
 
+  validates :title, :description, presence: true
+
 end
