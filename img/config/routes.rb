1f5859933354b0f7642c29e8202f3a1f17c1c578
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-11-18 17:15:50 -0800

    Initial test of user login & signup using basic Devise.

--- img/config/routes.rb
+++ img/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :image_users, :shallow=> true
     resources :tags, :shallow=> true
   end
+  root 'images#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
