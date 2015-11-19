18cfc0de84cedf8598b906b770a1f72afef57a8b
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-11-18 17:35:36 -0800

    Added NAME attribute to Devise Users table

--- img/db/schema.rb
+++ img/db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20151114234352) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
+    t.string   "name",                   default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
