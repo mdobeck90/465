e7eb1bf08c7148bf66771f11f56bbdd59669c662
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-11-21 12:46:10 -0800

    Added generate_filename to images model.

--- img/app/models/image.rb
+++ img/app/models/image.rb
@@ -5,5 +5,10 @@ class Image < ActiveRecord::Base
   has_many :Users, :through => :ImageUsers
 
   belongs_to :User
+
+  def generate_filename
+    chars = [('a'..'z'), (1..99)].map { |i| i.to_a }.flatten 
+    filename = (1..6).map {chars[rand(chars.length)]}.join
+  end
   
 end
