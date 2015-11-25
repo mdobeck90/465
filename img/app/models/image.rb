e7138ac27cdb0c42c4a6f9b01bcb7ef8744785a3
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-11-24 20:53:32 -0800

    User will now see recently uploaded files.  Overlooked a callback to before_save that was renaming the file after the previously generated name was saved to the database.

--- img/app/models/image.rb
+++ img/app/models/image.rb
@@ -4,12 +4,10 @@ class Image < ActiveRecord::Base
   has_many :tags
   has_many :users, :through => :ImageUsers
 
-  before_save :generate_filename
-
   belongs_to :user
 
   def generate_filename
-    chars = [('a'..'z'), (1..99)].map { |i| i.to_a }.flatten 
+    chars = [('a'..'z'), (1..9)].map { |i| i.to_a }.flatten 
     new_filename = (1..6).map { chars[rand(chars.length)] }.join
     new_filename = "#{new_filename}" + ".jpg"
     self.filename = new_filename
