de8114e90cdf5210aa5ed99c69630e61f2145d9a
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-12-04 10:52:32 -0800

    Got several things working: - The bcrypt gem. Seems this is MOST of what devise does... So I dunno how long these password gems last.  Shit must rollover quarterly. - passwords are hashed(sorta encrypted in the db) - passwords are filtered and cannot be seen in http posts - ^ I don't know how the filtering works. So I can't test around and see if there's more I need to do.  Mostly an, if theres time kinda thing - User account creation is successful.
    Need to:
    - Create users#edit page.  Need to give be able to change PW and add security questions
    - Create nav bar for ALL pages.  Want uniformity and the users#show should be the HOME screen
    - css styling for error messages and nav bar
    - create basic operatives to list

--- finalproject/app/models/user.rb
+++ finalproject/app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   has_many :targets, :through => :breaches
   has_many :inverse_breaches, :class_name => "Breaches", :foreign_key => "target_id"
   has_many :inverse_targets, :through => :inverse_breaches, :source => :user
+
+  has_secure_password
 end
--- finalproject/db/schema.rb
+++ finalproject/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204060841) do
+ActiveRecord::Schema.define(version: 20151204182929) do
 
   create_table "breaches", force: :cascade do |t|
     t.integer  "user_id"
@@ -28,8 +28,9 @@ ActiveRecord::Schema.define(version: 20151204060841) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
