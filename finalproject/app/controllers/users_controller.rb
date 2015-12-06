4c1f874b3096480289387ef5d38d2e8808f7e5fa
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-12-06 14:35:07 -0800

    Took all day but I can now have a button that does -1 from FIREWALL and +1 to ACTIVE FIREWALL.  Yayyy

--- finalproject/app/controllers/users_controller.rb
+++ finalproject/app/controllers/users_controller.rb
@@ -66,6 +66,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :password, :password_confirmation)
+      params.require(:user).permit(:name, :password, :password_confirmation, :firewall, :active_firewall)
     end
 end
