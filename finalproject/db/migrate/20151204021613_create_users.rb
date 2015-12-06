bb2d79d69b8b984d554080a165580c9a156a619b
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-12-06 12:13:43 -0800

    Added items to the user table.  Now the main menu is a breakdown of all operatives, items, and active defenses

--- finalproject/db/migrate/20151204021613_create_users.rb
+++ finalproject/db/migrate/20151204021613_create_users.rb
@@ -5,6 +5,12 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :password
       t.integer :cash, :default => 10000 
+      t.integer :zeroday, :default => 0
+      t.integer :o_contract, :default => 0
+      t.integer :honeypot, :default => 0
+      t.integer :firewall, :default => 0
+      t.integer :active_firewall, :default => 0
+      t.integer :active_honeypot, :default => 0
 
       t.timestamps null: false
     end
--- finalproject/db/schema.rb
+++ finalproject/db/schema.rb
@@ -35,6 +35,12 @@ ActiveRecord::Schema.define(version: 20151204224325) do
     t.string   "name"
     t.string   "password"
     t.integer  "cash",            default: 10000
+    t.integer  "zeroday",         default: 0
+    t.integer  "o_contract",      default: 0
+    t.integer  "honeypot",        default: 0
+    t.integer  "firewall",        default: 0
+    t.integer  "active_firewall", default: 0
+    t.integer  "active_honeypot", default: 0
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.string   "password_digest"
