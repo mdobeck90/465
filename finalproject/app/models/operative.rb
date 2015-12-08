e61b12a7014676b400d475d4fcb89a03b8304749
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-12-07 17:26:24 -0800

    Creating button that should call Operative's create action.

--- finalproject/app/models/operative.rb
+++ finalproject/app/models/operative.rb
@@ -1,4 +1,7 @@
 class Operative < ActiveRecord::Base
+  load "#{Rails.root}/lib/operative_f_name.rb"
+  load "#{Rails.root}/lib/operative_l_name.rb"
+
   belongs_to :user
 
   def check_deployment_time 
@@ -8,5 +11,10 @@ class Operative < ActiveRecord::Base
         self.status = "idle"
       end
     end
+  end
+
+  def generate_operative
+    self.user_id = current_user.id
+    self.name = f_name + " " + l_name
   end
 end
