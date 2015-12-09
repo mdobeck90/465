f7e0f4a7a476a8690c4a101f21886d224624962a
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-12-09 11:04:38 -0800

    Finished breach dice roll in models#breach.  Need to attach that outcome somewhere

--- finalproject/app/models/breach.rb
+++ finalproject/app/models/breach.rb
@@ -4,8 +4,29 @@ class Breach < ActiveRecord::Base
 
 
   def calculate_breach_outcome
-    honeypot_activate = User.find(self.target_id).active_honeypot
-    defense_rating = User.find(self.target_id).active_firewall
-    @zero_or_honey = defense_rating/rand(1..defense_rating)
+    honeypot_rating = User.find(self.target_id).active_honeypot + .0
+    defense_rating = User.find(self.target_id).active_firewall + .0
+  
+    honey_chance = rand(0.0..honeypot_rating)/honeypot_rating
+    defense_chance = rand(0.0..defense_rating)/defense_rating   
+    if honey_chance > defense_chance
+      @honeytrap = true
+    else
+      @honeytrap = false
+    end
+
+    if @honeytrap == true
+      if honey_chance > rand(0.0..1.0)
+        @repel = true
+      else
+        @repel = false
+      end
+    elsif @honeytrap == false
+      if defense_chance > rand(0.0..1.0)
+        @repel = true
+      else
+        @repel = false
+      end
+    end
   end
 end
