713f13ba38d2ab13eab7fbe6d69a42751c84abd4
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-12-09 10:31:56 -0800

    split up enemy breaches between succ/failure for user to view

--- finalproject/app/models/breach.rb
+++ finalproject/app/models/breach.rb
@@ -2,6 +2,7 @@ class Breach < ActiveRecord::Base
   belongs_to :user
   belongs_to :breach, :class_name => "User"
 
+
   def calculate_breach_outcome
     honeypot_activate = User.find(self.target_id).active_honeypot
     defense_rating = User.find(self.target_id).active_firewall
