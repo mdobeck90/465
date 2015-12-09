421b2e81fd9d410ace937860ecbf916a487df483
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-12-08 22:20:31 -0800

    Going to try to calculate breach success rate at breach creation. Will call a method placed inside breach#model.

--- finalproject/app/models/breach.rb
+++ finalproject/app/models/breach.rb
@@ -1,4 +1,10 @@
 class Breach < ActiveRecord::Base
   belongs_to :user
   belongs_to :breach, :class_name => "User"
+
+  def calculate_breach_outcome
+    honeypot_activate = User.find(self.target_id).active_honeypot
+    defense_rating = User.find(self.target_id).active_firewall
+    @zero_or_honey = defense_rating/rand(1..defense_rating)
+  end
 end
